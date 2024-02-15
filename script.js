90484efa687f0929dab7f6f20a85c5e8d5caefb0
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2024-02-15 22:36:51 +0530

    update files

---
+++ script.js
@@ -0,0 +1,9 @@
+const bgAnimation = document.getElementById('bgAnimation');
+
+const numberOfColorBoxes = 1600;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
