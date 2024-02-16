b82bd335208305d8f5425aad70bf040cc18f5525
HarshanaBandaraAg91 <139755295+HarshanaBandaraAg91@users.noreply.github.com>
2024-02-16 23:53:25 +0530

    remove files

--- script.js
+++
@@ -1,9 +0,0 @@
-const bgAnimation = document.getElementById('bgAnimation');
-
-const numberOfColorBoxes = 1600;
-
-for (let i = 0; i < numberOfColorBoxes; i++) {
-    const colorBox = document.createElement('div');
-    colorBox.classList.add('colorBox');
-    bgAnimation.append(colorBox)
-}
